39419c919637f0537a26ffe888832dee0cfdf30c
jasonexe <jkrein@calpoly.edu>
2017-06-08 13:26:20 -0700

    Initial start of the UI portion of correct

--- public/Correct/correctController.js
+++ public/Correct/correctController.js
@@ -1,1 +1,6 @@
 //Controller for correctly answered questions
+app.controller('categoryController',
+ ['$scope', '$state', 'anrs',
+    function($scope, $state, anrs) {
+       $scope.anrs = anrs;
+    }]);
