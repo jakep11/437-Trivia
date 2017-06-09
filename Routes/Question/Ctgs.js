9c20256ac5885bcf32c4340db8b64b3596637d8a
Jake Pickett <j.pickett67@yahoo.com>
2017-06-09 09:43:44 -0700

    Fixing deleting a category to delete questions too

--- Routes/Question/Ctgs.js
+++ Routes/Question/Ctgs.js
@@ -90,7 +90,7 @@ router.delete('/:ctgId', function(req, res) {
          cnn.chkQry('delete from Category where id = ?', [ctgId], cb);
    },
    function(nothing, fields, cb) {
-      cnn.chkQry('delete from Question where categoryId = ?', [ctgId], cb);
+      cnn.chkQry('delete from Question where categoryId = ?', [ctgId], cb); 
    }],
    function(err) {
       if (!err)
